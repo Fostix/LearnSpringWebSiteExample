ed55b9c0d668292cc975fa512b82d416457ddcf9
Albert <AccretionDiskThat@Gmail.com>
2023-03-02 15:18:15 +0500

    added add todo function

--- src/main/java/com/learn/webapp/todo/TodoService.java
+++ src/main/java/com/learn/webapp/todo/TodoService.java
@@ -10,17 +10,23 @@ import org.springframework.stereotype.Service;
 public class TodoService {
 	
 	private static List<Todo> todos = new ArrayList<>();
+	private static int todosCont = 0;
 	
 	static {
-		todos.add(new Todo(1, "user", "Learn AWS", 
+		todos.add(new Todo(++todosCont, "user", "Learn AWS", 
 				LocalDate.now().plusYears(1), false));
-		todos.add(new Todo(2, "user", "Learn DevOps", 
+		todos.add(new Todo(++todosCont, "user", "Learn DevOps", 
 				LocalDate.now().plusYears(2), false));
-		todos.add(new Todo(3, "user", "Learn Full Stack Development", 
+		todos.add(new Todo(++todosCont, "user", "Learn Full Stack Development", 
 				LocalDate.now().plusYears(3), false));
 	}
 	
 	public List<Todo> findByUsername(String username) {
 		return todos;
+	}
+	
+	public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+		Todo todo = new Todo(++todosCont, username, description, targetDate, done);
+		todos.add(todo);
 	}
 }
