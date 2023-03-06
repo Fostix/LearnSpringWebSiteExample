e7cec2c919c6e3108983f3697a03671f41dcea44
Albert <accretiondiskthat@gmail.com>
2023-03-06 17:53:40 +0500

    some refuctoring and added spring security

---
+++ src/main/java/com/learn/webapp/login/WelcomeController.java
@@ -0,0 +1,24 @@
+package com.learn.webapp.login;
+
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+//import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name") // For all controller can use this attribute!!
+public class WelcomeController {
+
+
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String gotoWelcomePage(ModelMap model) {
+		model.put("name", "user");
+		return "welcome";
+	}
+}
